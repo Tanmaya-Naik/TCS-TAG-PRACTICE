35643f8ce125765461ae323604abebddcb3c0866
Tanmaya <tanmayanaik2020@gmail.com>
2025-12-19 17:49:17 +0530

    optimised the product of dig

--- PREPINSTA QUESTION/product_of_digit.cpp
+++ PREPINSTA QUESTION/product_of_digit.cpp
@@ -10,6 +10,9 @@ int main(){
     int ans=1;
     while(temp > 0){
         int lastdigit=temp%10;
+        if(lastdigit == 0){
+            lastdigit=1;
+        }
         ans*=lastdigit;
         temp=temp/10;
     }
@@ -17,3 +20,24 @@ int main(){
     cout<<"The Product of all the digit of the number N is "<<ans<<endl;
     return 0;
 }
+
+
+//If asked take input as a string then
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     cout<<"Take input from the user: ";
+//     string s;
+//     cin>>s;
+//     int product=1;
+//     for(auto it:s){
+//         if(it == '0'){
+//             it='1';
+//         }
+//         product*=(it-'0');
+//     }
+
+//     cout<<"The price of the product is: "<<product<<endl;
+//     return 0;
+// }
