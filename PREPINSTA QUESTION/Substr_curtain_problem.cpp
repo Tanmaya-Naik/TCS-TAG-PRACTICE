7392146d869b531ef5f800168b4514da3076bd3a
Tanmaya <tanmayanaik2020@gmail.com>
2025-12-20 16:28:20 +0530

    Added the curtain problem

---
+++ PREPINSTA QUESTION/Substr_curtain_problem.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    string str="bbbaaababa";
+    int n=str.length();
+    int L=4;
+    vector<string> boxes;
+
+    for(int i=0;i<n;i+=L){
+        string put="";
+        for(int j=i;j< i+L && j < n;j++){
+            put+=str[j];
+        }
+
+        boxes.push_back(put);
+    }
+
+    string maxbox="";
+    int maxcnt=0;
+
+    for(string st:boxes){
+        int cnt=0;
+        for(char ch:st){
+            if(ch == 'a'){
+                cnt++;
+                
+            }
+        }
+
+        if(cnt > maxcnt){
+            maxcnt=cnt;
+            maxbox=st;
+        }
+    }
+
+    int finalcount=0;
+
+    for(char ch:maxbox){
+        if(ch=='a'){
+            finalcount++;
+        }
+    }
+
+    cout<<"The final box which contain maximun a is: "<<maxbox<<endl;
+    cout<<"The number of a present in the maxbox is: "<<finalcount<<endl;
+
+
+
+    return 0;
+}
